7312bf42053e3d1a64f7beab812c905a075ebcd9
jdasam <dasaem.jeong@gmail.com>
2023-08-20 23:33:03 +0900

    update files

--- midi_utils.py
+++ midi_utils.py
@@ -10,12 +10,18 @@ def load_midi_file(filename):
   return mid
 
 
-def get_midiout_channel():
+def get_midiout_channel(num_channels=1):
   midiout = rtmidi.MidiOut()
   available_ports = midiout.get_ports()
-  if available_ports:
-      midiout.open_port(0)
+  if available_ports and num_channels == 1:
+    midiout.open_port(0)
+    return [midiout]
+  if len(available_ports) >= num_channels:
+    midi_outs = [rtmidi.MidiOut() for i in range(num_channels)]
+    for i, midi_out in enumerate(midi_outs):
+      midi_out.open_port(i)
   else:
-      midiout.open_virtual_port("My virtual output")
-
-  return midiout
+    midi_outs = [rtmidi.MidiOut() for i in range(num_channels)]
+    for i, midi_out in enumerate(midi_outs):
+      midi_out.open_virtual_port(f"virtual_output_{i}")
+  return midi_outs
--- run.py
+++ run.py
@@ -1,33 +1,55 @@
 import time
 import midi_utils
 import mido
+import threading
 
+tempo_multiplier = 1.0
+dynamics_multiplier = 1.0
+now = time.time
 
-def main():
-  midi_obj = midi_utils.load_midi_file("test_pieces_Clair_de_Lune__Debussy_by_isgn.mid")
-
-  assert len(midi_obj.tracks) == 2
-  midi_events = midi_obj.tracks[1]
-
-  # midi_out = midi_utils.get_midiout_channel()
-  midi_out = mido.open_output()
+def play_midi(file_name):
+  global tempo_multiplier, dynamics_multiplier
+  midi_obj = midi_utils.load_midi_file(file_name)
+  midi_events = midi_obj.merged_track
+  midi_events = [event for event in midi_events if isinstance(event, mido.messages.messages.Message)]
+  midi_out = mido.open_output("Python MIDI", virtual=True)
   tempo = midi_obj.tracks[0][0].tempo
   scale = tempo * 1e-6 / midi_obj.ticks_per_beat
+  start_time = now()
+  input_time = 0.0
 
-  # midi_out.send(mido.MetaMessage('set_tempo', tempo=tempo))
+
   for event in midi_events:
-    time.sleep(event.time * scale)
+    input_time += event.time * scale * (1/tempo_multiplier)
+    if dynamics_multiplier != 1 and event.type == 'note_on' and event.velocity > 0:
+      event.velocity = min(max(64 + int( (event.velocity-64) * dynamics_multiplier), 1), 127)
+    playback_time = now() - start_time
+    duration_to_next_event = input_time - playback_time
+    if duration_to_next_event > 0.0:
+      time.sleep(duration_to_next_event)
     midi_out.send(event)
-    # midi_out.send_message(event.bytes())
-    # if event.type == 'note_on':
-    #   pitch = event.note
-    #   velocity = event.velocity
-    #   midi_out.send_message([0x90, pitch, velocity])
-    #   print("Note on: ", pitch, velocity)
-    # elif event.type == 'note_off':
-    #   pitch = event.note
-    #   midi_out.send_message([0x80, pitch, 0])
+  midi_obj.play()
 
+def control_input():
+  global tempo_multiplier, dynamics_multiplier
+  while True:
+    choice = input("Enter 't' to change tempo, 'd' to change dynamics, or 'q' to quit: ")
+    if choice.strip() == 't':
+      new_tempo = float(input("Enter new tempo multiplier (e.g., 0.5 for half speed, 2 for double speed): "))
+      tempo_multiplier = new_tempo
+    elif choice.strip() == 'd':
+      new_dynamics = float(input("Enter new dynamics multiplier (e.g., 0.5 for softer, 2 for louder): "))
+      dynamics_multiplier = new_dynamics
+    elif choice.strip() == 'q':
+      break
+
+def main():
+  midi_thread = threading.Thread(target=play_midi, args=("schumann_immersive_interpol.mid",))
+  midi_thread.start()
+
+  control_input()
+  midi_thread.join()
 
 if __name__ == '__main__':
   main()
+  # asyncio.run(main())
